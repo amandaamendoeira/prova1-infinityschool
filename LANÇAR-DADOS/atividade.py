4226ff7deaa1e930af96bcaa73c0e906fdd77368
Amanda Amendoeira <145858289+amandaamendoeira@users.noreply.github.com>
2024-09-22 10:45:14 -0300

    Add files via upload

---
+++ LANÇAR-DADOS/atividade.py
@@ -0,0 +1,10 @@
+import random
+
+def lancar_dados():
+    dado1 = random.randint(1, 6) 
+    dado2 = random.randint(1, 6)  
+    total = dado1 + dado2       
+    return total
+
+resultado = lancar_dados()
+print(f"O total dos dados lançados é: {resultado}")
