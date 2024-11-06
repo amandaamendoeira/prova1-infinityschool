aec484d90779e58612f5ae73614e95b43bff111b
Amanda Amendoeira <145858289+amandaamendoeira@users.noreply.github.com>
2024-11-05 21:45:47 -0300

    Update atividade1.py

--- FLET/atividade1.py
+++ FLET/atividade1.py
@@ -1,16 +1,13 @@
 import flet as ft
 
 def main(page: ft.Page):
-
     page.title = "Formulário de Contato"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
 
     nome_field = ft.TextField(label="Nome", autofocus=True, width=300)
     email_field = ft.TextField(label="Email", keyboard_type=ft.KeyboardType.EMAIL, width=300)
     mensagem_field = ft.TextField(label="Mensagem", multiline=True, width=300, height=150)
 
-    # Criando o botão de envio
     def on_submit(e):
 
         if nome_field.value and email_field.value and mensagem_field.value:
@@ -23,9 +20,7 @@ def main(page: ft.Page):
 
     submit_button = ft.ElevatedButton("Enviar", on_click=on_submit)
 
-
     confirmation_text = ft.Text("", size=20, color=ft.colors.GREEN)
-
 
     page.add(
         nome_field,
@@ -34,6 +29,5 @@ def main(page: ft.Page):
         submit_button,
         confirmation_text
     )
-
 
 ft.app(target=main)
